2675f5ec297a0785ace43bbb1d98c9089d31097e
Fox <576648759@qq.com>
2022-03-16 23:12:10 +0800

    23.06

---
+++ 淘宝/Taobao-10.10.5-Android-搜索.py
@@ -0,0 +1,9 @@
+'''
+@File    :   Taobao-10.10.5-Android-搜索.py    
+@APPTYPE :   Android
+@Model   :   Huawei P50
+
+@Modify Time      @Author    @Version    @Action
+------------      -------    --------    -----------
+2022/3/16 23:09   xyhu       10.10.5     None
+'''
