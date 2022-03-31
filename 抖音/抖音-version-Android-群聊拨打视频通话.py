6522da81598933094dd75ac8fc147cd4da9e9e14
Fox <576648759@qq.com>
2022-03-31 14:47:27 +0800

    新增动作

--- 抖音/抖音-version-Android-群聊拨打视频通话.py
+++ 抖音/抖音-version-Android-群聊拨打视频通话.py
@@ -7,3 +7,51 @@
 ------------      -------    --------    -----------
 2022/3/29 15:21   xyhu       20.0.0      None
 '''
+import time
+from appium.webdriver.common.touch_action import TouchAction
+from selenium.webdriver.common.by import By
+import 抖音_Base as DouYin
+
+
+def group_call_video():
+    driver = DouYin.driver
+    try:
+        el = driver.find_element(By.XPATH, '//android.widget.Button[@text="我知道了"]')
+        el.click()
+        time.sleep(2)
+    except:
+        pass
+    try:
+        el0 = driver.find_element(By.XPATH, '//android.view.View[@content-desc="点击进入直播间"]')
+        if el0:
+            time.sleep(2)
+    except:
+        TouchAction(driver).tap(x=600, y=1268).perform().release()
+    time.sleep(3)
+    el1 = driver.find_element(By.XPATH, '//android.widget.TextView[@content-desc="消息，按钮"]')
+    el1.click()
+    time.sleep(3)
+    el2 = driver.find_element(By.XPATH, '//android.widget.ImageView[@content-desc="创建群聊"]')
+    el2.click()
+    time.sleep(2)
+    el3 = driver.find_element(By.XPATH, '//android.widget.TextView[@text="已加入的群聊"]')
+    el3.click()
+    time.sleep(2)
+    el4 = driver.find_element(By.XPATH, '//android.widget.TextView[@text="A测试"]')
+    el4.click()
+    time.sleep(2)
+    el5 = driver.find_element(By.XPATH, '//android.widget.ImageView[@content-desc="更多面板"]')
+    el5.click()
+    time.sleep(2)
+    el6 = driver.find_element(By.XPATH, '//android.widget.TextView[@text="视频通话"]')
+    el6.click()
+    time.sleep(5)
+    el7 = driver.find_element(By.XPATH, '//android.widget.TextView[@text="A测试"]')
+    el7.click()
+    time.sleep(2)
+    el8 = driver.find_element(By.XPATH, '//android.widget.TextView[contains(@text,"邀请")]')
+    el8.click()
+    time.sleep(5)
+
+
+group_call_video()
